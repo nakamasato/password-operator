50218e0ab92d36d7100b88dbfb2c2682d67218f5
Naka Masato <masatonaka1989@gmail.com>
2022-05-16 06:51:05 +0900

    [API] Remove Foo field from custom resource Password

--- api/v1alpha1/password_types.go
+++ api/v1alpha1/password_types.go
@@ -28,8 +28,6 @@ type PasswordSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Password. Edit password_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
 }
 
 // PasswordStatus defines the observed state of Password
