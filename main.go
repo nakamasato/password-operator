97e7e1d2cb636645fac16d6dd1c3767ddb465eaf
Masato Naka <masatonaka1989@gmail.com>
2022-11-09 21:53:13 +0900

    [kubebuilder] Create validating admission webhook

--- main.go
+++ main.go
@@ -96,6 +96,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Password")
 		os.Exit(1)
 	}
+	if err = (&secretv1alpha1.Password{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Password")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
