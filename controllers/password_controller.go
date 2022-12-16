ce3ebde1dfb58ddc09533bb227f2364363f2873d
Naka Masato <masatonaka1989@gmail.com>
2022-12-16 21:42:33 +0900

    Upgrade kubebuilder to 3.8.0 (#7)

--- controllers/password_controller.go
+++ controllers/password_controller.go
@@ -51,7 +51,7 @@ type PasswordReconciler struct {
 // the user.
 //
 // For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.1/pkg/reconcile
 func (r *PasswordReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
 
