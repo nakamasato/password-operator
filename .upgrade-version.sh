0201f8b4b7d458d3770b8db2971a4255f24ca13f
Masato Naka <masatonaka1989@gmail.com>
2023-07-28 06:31:41 +0900

    Remove all files to upgrade versions (v3.10.0)

--- .upgrade-version.sh
+++ .upgrade-version.sh
@@ -55,6 +55,7 @@ KEEP_FILES=(
     renovate.json
 )
 
+sudo rm -rf bin
 rm -rf api config controllers hack bin bundle cmd internal
 for f in `ls` .dockerignore .gitignore; do
     if [[ ! " ${KEEP_FILES[*]} " =~ " ${f} " ]] && [ -f "$f" ]; then
