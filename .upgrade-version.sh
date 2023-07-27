2f5f3d3baa9c689a7760fc3a4c3ee03334f9adcb
Masato Naka <masatonaka1989@gmail.com>
2023-07-27 09:02:36 +0900

    Remove all files to upgrade versions (v3.10.0)

--- .upgrade-version.sh
+++ .upgrade-version.sh
@@ -7,7 +7,6 @@ PASSWORD_GO_TYPE_FILE=api/v1alpha1/password_types.go
 PASSWORD_WEBHOOK_FILE=api/v1alpha1/password_webhook.go
 SAMPLE_YAML_FILE=config/samples/secret_v1alpha1_password.yaml
 CERT_MANAGER_VERSION=v1.8.0
-export KUSTOMIZE_VERSION=v4.5.5
 
 pre-commit
 get_latest_release() {
