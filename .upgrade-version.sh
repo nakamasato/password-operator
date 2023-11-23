63a0ea0d50b93cfad30a7587cbfe17306ec6db54
Masato Naka <masatonaka1989@gmail.com>
2023-11-23 17:18:09 +0900

    fix

--- .upgrade-version.sh
+++ .upgrade-version.sh
@@ -524,6 +524,7 @@ make undeploy
 kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/$CERT_MANAGER_VERSION/cert-manager.yaml
 
 git add .
+golangci-lint run ./... --fix
 pre-commit run -a || true
 git add . && git commit -am "[API] Implement validating admission webhook"
 
